7fc6f62a2bbca0ba60185f98d1e2739c453db75e
missou <695383649@qq.com>
2021-05-02 17:07:07 +0800

    add picom and gitui config

--- scripts/videowallpaper.sh
+++ scripts/videowallpaper.sh
@@ -2,3 +2,4 @@
 
 rm /tmp/wallpaper
 mkfifo /tmp/wallpaper; xwinwrap -fs -nf -fdt -ov -- mplayer -shuffle -slave -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc -nosound "/data/Video/视频壁纸/Wallpaper.mp4"
+#feh --bg-fill /data/Picture/壁纸/monknow/wallpaper8.jpg
