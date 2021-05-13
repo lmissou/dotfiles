27b0ce3032e2302354dc2dac6cb48fed17161275
missou <695383649@qq.com>
2021-05-13 21:34:59 +0800

    update

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -2,4 +2,4 @@
 
 rm /tmp/wallpaper
 mkfifo /tmp/wallpaper; xwinwrap -fs -nf -fdt -ov -- mplayer -shuffle -slave -input file=/tmp/wallpaper -loop 0 -wid WID -nolirc -nosound "/data/Video/视频壁纸/合成 1.mp4"
-#feh --bg-fill /data/Picture/壁纸/monknow/wallpaper8.jpg
+#feh --bg-fill ~/wallpaper/wallpaper.jpg
