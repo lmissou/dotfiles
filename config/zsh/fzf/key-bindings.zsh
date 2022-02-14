710d88e34f5763bbd51414193e7ec80007007686
missou <695383649@qq.com>
2022-02-14 21:17:32 +0800

    update fzf keybinding

--- config/zsh/fzf/key-bindings.zsh
+++ config/zsh/fzf/key-bindings.zsh
@@ -38,7 +38,6 @@ fi
 
 [[ -o interactive ]] || return 0
 
-# CTRL-G - Paste the selected file path(s) into the command line
 __fsel() {
   local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type f -print \
@@ -66,9 +65,7 @@ fzf-file-widget() {
   return $ret
 }
 zle     -N   fzf-file-widget
-bindkey '^G' fzf-file-widget
 
-# ALT-C - cd into the selected directory
 fzf-cd-widget() {
   local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type d -print 2> /dev/null | cut -b3-"}"
@@ -87,9 +84,7 @@ fzf-cd-widget() {
   return $ret
 }
 zle     -N    fzf-cd-widget
-bindkey '\ec' fzf-cd-widget
 
-# CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
@@ -106,7 +101,6 @@ fzf-history-widget() {
   return $ret
 }
 zle     -N   fzf-history-widget
-bindkey '^R' fzf-history-widget
 
 } always {
   eval $__fzf_key_bindings_options
--- config/zsh/keys.zsh
+++ config/zsh/keys.zsh
@@ -5,5 +5,11 @@
 
 # emacs keybinding
 bindkey -e #emacs
-# tmux
+# CTRL-R - Paste the selected command from history into the command line
+bindkey '^R' fzf-history-widget
+# ALT-D - Paste the selected file path(s) into the command line
+bindkey '\ed' fzf-file-widget
+# CTRL-G - goto (cd) into the selected directory
+bindkey '^G' fzf-cd-widget
+# C-T attach tmux
 bindkey -s '^T' 'tmux attach\n'
