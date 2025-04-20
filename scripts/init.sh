baa86d5669f193a717f19b04ec8efa534212bc8a
missou <695383649@qq.com>
2025-04-20 11:43:50 +0800

    update config

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# create symbolink for config files
-
+##### create symbolink for config files
 DOT_PATH=$PWD
 CONFIG_PATH=${XDG_CONFIG_HOME:-$HOME/tmp}
-
 read -r -p "Are You Sure? [Y/n] " input
-
 case $input in
   [yY][eE][sS]|[yY])
     ;;
@@ -14,7 +11,6 @@ case $input in
     exit 0
     ;;
 esac
-
 for file in `ls $DOT_PATH/config`
 do
   if [[ -d "$CONFIG_PATH/$file" || -f "$CONFIG_PATH/$file" ]]; then
@@ -22,4 +18,15 @@ do
   fi
   ln -s "$DOT_PATH/config/$file" "$CONFIG_PATH/"
 done
+
+
+##### setup somethings
+# use english name for user dirs
+LANG=en_US.UTF-8 xdg-user-dirs-update
+
+# install systemd services
+systemctl --user enable hyprpolkitagent.service
+systemctl --user enable hypridle.service
+systemctl --user enable waybar.service
+systemctl --user enable swaync.service
 
--- scripts/systemd-enable.sh
+++
@@ -1,4 +0,0 @@
-systemctl --user enable hyprpolkitagent.service
-systemctl --user enable hypridle.service
-systemctl --user enable waybar.service
-systemctl --user enable swaync.service
