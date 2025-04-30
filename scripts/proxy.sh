cf7e0df7991320928a50ce4a7fa409e7d4f1eb70
missou <695383649@qq.com>
2025-04-30 19:23:08 +0800

    update proxy.sh

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 代理打开时设置代理
-if [ "$(curl -sL -w '%{http_code}' http://127.0.0.1:7897 -o /dev/null)" != "000" ]; then
-  export https_proxy=http://127.0.0.1:7897
-  export http_proxy=http://127.0.0.1:7897
-  export all_proxy=socks5://127.0.0.1:7897
+if [ "$(curl -sL -w '%{http_code}' http://127.0.0.1:7890 -o /dev/null)" != "000" ]; then
+  export https_proxy=http://127.0.0.1:7890
+  export http_proxy=http://127.0.0.1:7890
+  export all_proxy=socks5://127.0.0.1:7890
 else
   unset https_proxy
   unset http_proxy
